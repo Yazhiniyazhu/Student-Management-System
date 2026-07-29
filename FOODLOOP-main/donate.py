8d411bc2d82f687ee63893bf587e5ffb315fa363
Yazhiniyazhu <yazhini.g2024@vitstudent.ac.in>
2026-07-29 17:22:32 +0530

    initial foodloop

---
+++ FOODLOOP-main/donate.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("🍲 Donate Food")
+
+food = st.text_input("Food Name")
+
+quantity = st.number_input(
+    "Quantity",
+    min_value=1
+)
+
+expiry = st.date_input("Expiry Date")
+
+location = st.text_area("Pickup Address")
+
+if st.button("Donate"):
+
+    st.success("Food Donation Added Successfully!")
